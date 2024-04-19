e945c7a2c00272cdd60016e94fef7162866d3697
Vedant Kansara <vedant@vedants-mac.dyn.nyp.org>
2024-04-19 16:13:22 -0400

    Initial Commit

---
+++ R/api_pull.R
@@ -0,0 +1,78 @@
+library(tidyverse)
+
+pull_redcap_data <- function(data = "reconfig"){
+
+  ### Pulling raw data
+  formData_raw <- list("token"=token,
+                       content='record',
+                       action='export',
+                       format='csv',
+                       type='flat',
+                       csvDelimiter='',
+                       rawOrLabel='raw',
+                       rawOrLabelHeaders='raw',
+                       exportCheckboxLabel='false',
+                       exportSurveyFields='false',
+                       exportDataAccessGroups='false',
+                       returnFormat='csv'
+  )
+  response_raw <- httr::POST(url, body = formData_raw, encode = "form")
+  result_raw <- httr::content(response_raw)
+
+  ### Pulling labelled data
+  formData_label <- list("token"=token,
+                         content='record',
+                         action='export',
+                         format='csv',
+                         type='flat',
+                         csvDelimiter='',
+                         rawOrLabel='label',
+                         rawOrLabelHeaders='raw',
+                         exportCheckboxLabel='true',
+                         exportSurveyFields='false',
+                         exportDataAccessGroups='false',
+                         returnFormat='csv'
+  )
+  response_label <- httr::POST(url, body = formData_label, encode = "form")
+  result_label <- httr::content(response_label)
+
+  ###Pulling data dictionary
+  formData_dict <- list("token"=token,
+                        content='metadata',
+                        format='csv',
+                        returnFormat='csv'
+  )
+  response_dict <- httr::POST(url, body = formData_dict, encode = "form")
+  data_dict <- httr::content(response_dict)
+
+  ### Get radio and dropdown column names
+  fact_cols <- data_dict %>%
+    filter(field_type %in% c("radio", "dropdown")) %>%
+    pull(field_name)
+
+  ### Get checkbox column names
+  chec_box_cols <- data_dict %>%
+    filter(field_type %in% c("checkbox")) %>%
+    pull(field_name)
+
+  ### Select those columns from labelled data and rename each column
+  ### by adding .factor at end
+  fact_df <- result_label %>%
+    select(all_of(fact_cols), all_of(starts_with(chec_box_cols))) %>%
+    rename_with( ~ paste0(., ".factor"), everything())
+
+  ### Combine the factored columns with raw data
+  df <- result_raw %>%
+    bind_cols(fact_df)
+
+  return(df)
+}
+
+
+
+
+
+
+
+
+
---
+++ R/my_functions.R
@@ -0,0 +1,187 @@
+library(tidyverse)
+
+### Function to get gose from all the data
+
+gose_func <- function(data){
+  df <- data %>%
+    select(record_id, wlst, discharge_gose, follow_up_period.factor, gose) %>%
+    group_by(record_id) %>%
+    fill(discharge_gose, .direction = "updown") %>%
+    fill(wlst, .direction = "updown") %>%
+    filter(!(is.na(follow_up_period.factor) & is.na(gose))) %>%
+    mutate(gose_3m = ifelse(follow_up_period.factor == "3 Month", gose, NA),
+           gose_6m = ifelse(follow_up_period.factor == "6 Month", gose, NA),
+           gose_12m = ifelse(follow_up_period.factor == "12 Month", gose, NA),
+           gose_ot = ifelse(follow_up_period.factor == "Other", gose, NA)) %>%
+    fill(gose_3m, .direction = "updown") %>%
+    fill(gose_6m, .direction = "updown") %>%
+    fill(gose_12m, .direction = "updown") %>%
+    fill(gose_ot, .direction = "updown") %>%
+    select(-gose, -follow_up_period.factor) %>%
+    distinct() %>%
+    ungroup() %>%
+    mutate(best_gose = pmax(discharge_gose, gose_3m, gose_6m, gose_12, gose_ot, na.rm = TRUE))
+
+  return(df)
+}
+
+
+mrs_func <- function(data){
+  df <- data %>%
+    select(record_id, wlst, discharge_mrs, follow_up_period.factor, mrs) %>%
+    group_by(record_id) %>%
+    fill(discharge_mrs, .direction = "updown") %>%
+    fill(wlst, .direction = "updown") %>%
+    filter(!(is.na(follow_up_period.factor) & is.na(mrs))) %>%
+    mutate(mrs_3m = ifelse(follow_up_period.factor == "3 Month", mrs, NA),
+           mrs_6m = ifelse(follow_up_period.factor == "6 Month", mrs, NA),
+           mrs_12m = ifelse(follow_up_period.factor == "12 Month", mrs, NA),
+           mrs_ot = ifelse(follow_up_period.factor == "Other", mrs, NA)) %>%
+    fill(mrs_3m, .direction = "updown") %>%
+    fill(mrs_6m, .direction = "updown") %>%
+    fill(mrs_12m, .direction = "updown") %>%
+    fill(mrs_ot, .direction = "updown") %>%
+    select(-mrs, -follow_up_period.factor) %>%
+    distinct() %>%
+    ungroup() %>%
+    mutate(best_mrs = pmin(discharge_mrs, mrs_3m, mrs_6m, mrs_12m, mrs_ot, na.rm = TRUE))
+
+  return(df)
+}
+
+
+command_score_func <- function(data, date = "test_datetime", time = "test_datetime", type = "all"){
+
+  if(date == time){
+    df <- data %>%
+      select(record_id, !!sym(date), command_score) %>%
+      filter(!(is.na(!!sym(date)) & is.na(command_score) )) %>%
+      mutate(date = as.Date(stringr::str_sub(!!sym(date), 1, 10)),
+             time = stringr::str_sub(!!sym(date), 12, ),
+             .after = !!sym(date)) %>%
+      select(-!!sym(date)) %>%
+      group_by(record_id) %>%
+      arrange(date, .by_group = TRUE) %>%
+      mutate(command_yn = ifelse(any(command_score >= 4), 1, 0)) %>%
+      ungroup()
+  }
+  else{
+    df <- data %>%
+      select(record_id, !!sym(date), !!sym(time), command_score) %>%
+      filter(!(is.na(!!sym(date)) & is.na(!!sym(time)) & is.na(command_score))) %>%
+      group_by(record_id) %>%
+      rename(date = !!sym(date),
+             time = !!sym(time)) %>%
+      arrange(date, .by_group = TRUE) %>%
+      mutate(command_yn = ifelse(any(command_score >= 4), 1, 0)) %>%
+      ungroup()
+  }
+
+  if(type == "raw"){
+    df <- df %>%
+      select(-command_yn)
+  }
+  else if(type == "yesno"){
+    df <- df %>%
+      select(record_id, command_yn) %>%
+      group_by(record_id) %>%
+      distinct() %>%
+      ungroup()
+  }
+  else if(type == "all"){
+    df <- df
+  }
+
+  return(df)
+}
+
+
+crsr_func <- function(data, date = "test_datetime", time = "test_datetime", type = "all"){
+
+  if(date == time){
+    df <- data %>%
+      select(record_id, !!sym(date), crsr_auditory, crsr_visual, crsr_motor,
+             crsr_oromotor_verbal, crsr_communication, crsr_arousal, crsr_total) %>%
+      filter(!is.na(crsr_total)) %>%
+      mutate(date = as.Date(stringr::str_sub(!!sym(date), 1, 10)),
+             time = stringr::str_sub(!!sym(time), 12, ),
+             .after = !!sym(date)) %>%
+      select(-!!sym(date))
+  }
+  else{
+    df <- data %>%
+      select(record_id, !!sym(date), !!sym(time), crsr_auditory, crsr_visual, crsr_motor,
+             crsr_oromotor_verbal, crsr_communication, crsr_arousal, crsr_total) %>%
+      filter(!is.na(crsr_total)) %>%
+      rename(date = !!sym(date),
+             time = !!sym(time))
+  }
+
+  df <- df %>%
+    mutate(cs_group = case_when(crsr_arousal == 0 & crsr_auditory < 3 &
+                                  crsr_visual < 2 & crsr_motor < 3 &
+                                  crsr_oromotor_verbal <= 1 & crsr_communication == 0 ~ "Coma",
+                                crsr_arousal > 0 & crsr_auditory < 3 &
+                                  crsr_visual < 2 & crsr_motor < 3 &
+                                  crsr_oromotor_verbal < 3 & crsr_communication < 1 ~ "VS",
+                                (crsr_visual > 1 | (crsr_motor > 2 & crsr_motor < 6)) &
+                                  (crsr_auditory < 3 & crsr_oromotor_verbal < 3 &
+                                     crsr_communication < 1) ~ "MCSm",
+                                (crsr_auditory >= 3 | crsr_oromotor_verbal == 3 |
+                                   crsr_communication == 1) &
+                                  (crsr_communication < 2 & crsr_motor < 6) ~ "MCSp",
+                                crsr_communication == 2 | crsr_motor == 6 ~ "CS")) %>%
+    select(record_id, date, time, crsr_total, cs_group) %>%
+    group_by(record_id) %>%
+    arrange(date, .by_group = TRUE) %>%
+    mutate(crsr_yn = ifelse(any(crsr_total >= 8), 1, 0),
+           .after = crsr_total)
+
+  if(type == "raw"){
+    df <- df %>%
+      select(-crsr_yn)
+  }
+  else if(type == "yesno"){
+    df <- df %>%
+      select(record_id, crsr_yn) %>%
+      group_by(record_id) %>%
+      distinct() %>%
+      ungroup()
+  }
+  else if(type == "all"){
+    df <- df
+  }
+
+  return(df)
+
+}
+
+demo_func <- function(data, database = "consciousness"){
+
+  if(database == "consciousness"){
+    etiology <- "primary_adm_dx"
+  }
+  else if(database == "reconfig" | database == "aphasia"){
+    etiology <- "ich_etiology_smashu"
+  }
+
+  df <- data %>%
+    select(record_id, dob, sex.factor, race.factor, handedness.factor, years_of_education.factor,
+           primary_adm_dx_onset, (ends_with("_date") & contains(c("hosp", "icu"))), !!sym(etiology) ) %>%
+    group_by(record_id) %>%
+    fill(all_of(colnames(.)[-1])) %>%
+    distinct() %>%
+    ungroup() %>%
+    mutate(age = as.numeric(difftime(primary_adm_dx_onset, dob, units = "days")/365.25),
+           .after = dob)
+
+  if(length(unique(df$record_id)) == nrow(df) ){
+    return(df)
+  }
+  else{
+    return(NA)
+  }
+
+}
+
+
